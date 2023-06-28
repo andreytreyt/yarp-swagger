6701985a618197c98e64cd892ee9407865b17450
Andrei Tsaregorodtsev <andreytreyt@hotmail.com>
2023-06-28 10:26:34 +0300

    Update ReverseProxyDocumentFilterConfig.cs

--- src/Yarp.ReverseProxy.Swagger/ReverseProxyDocumentFilterConfig.cs
+++ src/Yarp.ReverseProxy.Swagger/ReverseProxyDocumentFilterConfig.cs
@@ -5,27 +5,31 @@ namespace Yarp.ReverseProxy.Swagger
 {
     public sealed class ReverseProxyDocumentFilterConfig
     {
-        public IReadOnlyDictionary<string, Cluster> Clusters { get; init; } = new Dictionary<string, Cluster>();
+        public IReadOnlyDictionary<string, Cluster> Clusters { get; init; }
 
         public sealed class Cluster
         {
-            public IReadOnlyDictionary<string, Destination> Destinations { get; init; } = new Dictionary<string,Destination>();
+            public IReadOnlyDictionary<string, Destination> Destinations { get; init; }
 
             public sealed class Destination
             {
                 public string AccessTokenClientName { get; init; }
                 public string Address { get; init; }
-                public IReadOnlyList<Swagger> Swaggers { get; init; } = new List<Swagger>();
+                public IReadOnlyList<Swagger> Swaggers { get; init; }
 
                 public sealed class Swagger
                 {
                     public string PrefixPath { get; init; }
-                    public IReadOnlyList<string> Paths { get; init; } = new List<string>();
+                    public IReadOnlyList<string> Paths { get; init; }
                 }
             }
         }
-
-        public static ReverseProxyDocumentFilterConfig Empty => new ReverseProxyDocumentFilterConfig();
+        
         public bool IsEmpty => Clusters?.Any() != true;
+        
+        public static ReverseProxyDocumentFilterConfig Empty => new ReverseProxyDocumentFilterConfig
+        {
+            Clusters = new Dictionary<string, Cluster>()
+        };
     }
 }
