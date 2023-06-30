3adb4f723bd7a9d11f693b579968e7a7c0f5dfe3
Andrei Tsaregorodtsev <andreytreyt@hotmail.com>
2023-06-30 10:05:13 +0300

    Update ReverseProxyDocumentFilterConfig.cs

--- src/Yarp.ReverseProxy.Swagger/ReverseProxyDocumentFilterConfig.cs
+++ src/Yarp.ReverseProxy.Swagger/ReverseProxyDocumentFilterConfig.cs
@@ -5,22 +5,22 @@ namespace Yarp.ReverseProxy.Swagger
 {
     public sealed class ReverseProxyDocumentFilterConfig
     {
-        public IReadOnlyDictionary<string, Cluster> Clusters { get; init; } = new Dictionary<string, Cluster>();
+        public IReadOnlyDictionary<string, Cluster> Clusters { get; init; }
 
         public sealed class Cluster
         {
-            public IReadOnlyDictionary<string, Destination> Destinations { get; init; } = new Dictionary<string,Destination>();
+            public IReadOnlyDictionary<string, Destination> Destinations { get; init; }
 
             public sealed class Destination
             {
                 public string AccessTokenClientName { get; init; }
                 public string Address { get; init; }
-                public IReadOnlyList<Swagger> Swaggers { get; init; } = new List<Swagger>();
+                public IReadOnlyList<Swagger> Swaggers { get; init; }
 
                 public sealed class Swagger
                 {
                     public string PrefixPath { get; init; }
-                    public IReadOnlyList<string> Paths { get; init; } = new List<string>();
+                    public IReadOnlyList<string> Paths { get; init; }
                 }
             }
         }
