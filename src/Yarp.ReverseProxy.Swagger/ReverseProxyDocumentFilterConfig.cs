8f61d2cde7c1d63c79aadb6dc8171706c6a09bf0
Wayne Bowie <lilwayne1556@gmail.com>
2024-12-26 10:56:16 -0600

    Handle duplicate schemas when using common document (#44)
    * Added RenameDuplicateComponents to swagger config
    
    * Simplified code
    
    * Variable clarity
    
    ---------
    
    Co-authored-by: Wayne Bowie <wayneb@pcius.com>

--- src/Yarp.ReverseProxy.Swagger/ReverseProxyDocumentFilterConfig.cs
+++ src/Yarp.ReverseProxy.Swagger/ReverseProxyDocumentFilterConfig.cs
@@ -35,6 +35,7 @@ namespace Yarp.ReverseProxy.Swagger
         {
             public bool IsCommonDocument { get; set; } = false;
             public string CommonDocumentName { get; set; } = "YARP";
+            public bool RenameDuplicateSchemas { get; set; } = false;
         }
 
         public bool IsEmpty => Clusters?.Any() != true;
